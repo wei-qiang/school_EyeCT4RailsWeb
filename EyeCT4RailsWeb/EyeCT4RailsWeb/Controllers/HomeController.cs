cad478dd9a2ad0def5c7cad3f8140a2305aa2f70
Dennis Vermeulen <d.vermeulen@me.com>
2016-12-15 20:05:34 +0100

    A begin is made with the Login page

--- EyeCT4RailsWeb/EyeCT4RailsWeb/Controllers/HomeController.cs
+++ EyeCT4RailsWeb/EyeCT4RailsWeb/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EyeCT4RailsWeb.Data;
+using EyeCT4RailsWeb.Logic;
+using EyeCT4RailsWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,27 @@ namespace EyeCT4RailsWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private GebruikerRepository GebruikerRepo = new GebruikerRepository(new GebruikerMSSQLContext());
+
         // GET: Home
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Login(string gebruikersnaam, string wachtwoord)
+        {
+            Gebruiker gebruiker = GebruikerRepo.LoginUser(gebruikersnaam, wachtwoord);
+
+            if(gebruiker == null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Overzicht");
+                //Hier moet nog het hele session gebeuren omheen gebouwd worden
+            }
         }
     }
 }
