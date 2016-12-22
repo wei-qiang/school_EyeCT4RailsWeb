480551bf0f1cdb95f4bb36b6e548eafa8fdcdf56
wei-qiang <wei-qiang@hotmail.nl>
2016-12-22 14:09:19 +0100

    updated trammsqlcontext

--- EyeCT4RailsWeb/EyeCT4RailsWeb/Models/Tram.cs
+++ EyeCT4RailsWeb/EyeCT4RailsWeb/Models/Tram.cs
@@ -40,14 +40,15 @@ namespace EyeCT4RailsWeb.Models
         /// <summary>
         /// constructor van tram die geen sector heeft
         /// </summary>
-        public Tram(int _ID, string _Tramtype, int _Lijn, Status _Status/*, int _PrioriteitReparatie, int _PrioriteitSchoonmaak*/)
+        public Tram(int _ID, string _Tramtype, int _Tramnummer, int _Lijn, Status _Status, int _PrioriteitReparatie, int _PrioriteitSchoonmaak)
         {
             ID = _ID;
             Tramtype = _Tramtype;
+            TramNummer = _Tramnummer;
             Lijn = _Lijn;
             Status = _Status;
-            //PrioriteitReparatie = _PrioriteitReparatie;
-            //PrioriteitSchoonmaak = _PrioriteitSchoonmaak;
+            PrioriteitReparatie = _PrioriteitReparatie;
+            PrioriteitSchoonmaak = _PrioriteitSchoonmaak;
         }
         public Tram(int _ID, string _Tramtype, int _TramNummer, Status _Status, int _Sector, int _Spoor/*, int _PrioriteitReparatie, int _PrioriteitSchoonmaak*/)
         {
@@ -58,21 +59,23 @@ namespace EyeCT4RailsWeb.Models
             Sector = new Sector(_Spoor, _Sector);
         }
 
-        public Tram(int _ID, string _Tramtype)
+        public Tram(int _ID, int _tramnummer, string _Tramtype)
         {
             ID = _ID;
+            TramNummer = _tramnummer;
             Tramtype = _Tramtype;
         }
 
         /// <summary>
         /// deze constructor maakt een instantie van de tram aan en haalt zelf de sector klasse op als je alleen het nummer doorgeeft
         /// </summary>
-        public Tram(int _ID, string _Tramtype, int _Lijn, int _Sector, Status _Status/*, int _PrioriteitReparatie, int _PrioriteitSchoonmaak*/)
+        public Tram(int _ID, string _Tramtype, int _Tramnummer, int _Lijn, int _Spoor, int _Sector, Status _Status, int _PrioriteitReparatie, int _PrioriteitSchoonmaak)
         {
             ID = _ID;
             Tramtype = _Tramtype;
+            TramNummer = _Tramnummer;
             Lijn = _Lijn;
-            this.Sector = sectorRepo.GetSectorByID(_Sector);
+            this.Sector = sectorRepo.GetSectorByID(_Spoor,_Sector);
             Status = _Status;
             //PrioriteitReparatie = _PrioriteitReparatie;
             //PrioriteitSchoonmaak = _PrioriteitSchoonmaak;
