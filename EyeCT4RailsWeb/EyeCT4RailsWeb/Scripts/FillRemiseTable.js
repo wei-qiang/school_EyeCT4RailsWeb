1987b85be096c2f49442609fe12a23bd3a38e4ea
Teun van Laarhoven <teunvanlaarhoven@gmail.com>
2017-01-10 12:55:20 +0100

    blokkeren van sector gebeurd ook als er een tram opstaat

--- EyeCT4RailsWeb/EyeCT4RailsWeb/Scripts/FillRemiseTable.js
+++ EyeCT4RailsWeb/EyeCT4RailsWeb/Scripts/FillRemiseTable.js
@@ -4,6 +4,7 @@
         for (var i = 0; i < data.length; i++) {
             var id = "#sector" + data[i].Sector.Spoor.toString() + data[i].Sector.Nummer.toString();
             $(id).text(data[i].TramNummer);
+            $(id).addClass("blocked");
         }
         alert("trams loaded");
     });
